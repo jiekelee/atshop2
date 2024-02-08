4e93c9e2f999d1cd54fbbbbc482cca9bf9ca3610
jack <jieke918@gmail.com>
2024-02-08 16:47:27 +0800

    test: testpush

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ str }}</h1>
+
+  </div>
+</template>
+
+<script setup lang="ts" name="App">
+
+let str="this is test str";
+console.log(str);
+
+</script>
+
+<style lang="scss" scoped>
+h1{
+  background: skyblue;
+}
+
+</style>
