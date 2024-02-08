1dbe2e46cf30aeb2b9161812b34b42f074cab742
jack <jieke918@gmail.com>
2024-02-08 16:52:12 +0800

    test testfirst

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <template>
   <div>
     <h1>{{ str }}</h1>
-
   </div>
 </template>
 
 <script setup lang="ts" name="App">
-
-let str="this is test str";
-console.log(str);
-
+let str = 'this is test str'
+console.log(str)
 </script>
 
 <style lang="scss" scoped>
-h1{
+h1 {
   background: skyblue;
 }
-
 </style>
